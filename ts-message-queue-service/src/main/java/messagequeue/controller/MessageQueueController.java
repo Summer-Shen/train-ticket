1852d68b9177554f84bebd9c431c72249572ed19
Summer-Shen <1563407190@qq.com>
2021-07-17 15:49:44 +0800

    implement basic mq service controller

--- ts-message-queue-service/src/main/java/messagequeue/controller/MessageQueueController.java
+++ ts-message-queue-service/src/main/java/messagequeue/controller/MessageQueueController.java
@@ -1,4 +1,43 @@
 package messagequeue.controller;
 
+import messagequeue.entity.NotifyInfo;
+import messagequeue.service.NotificationServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author Yunfei Shen
+ */
+@RestController
+@RequestMapping("/api/v1/mqservice")
 public class MessageQueueController {
+
+    private final NotificationServiceImpl service;
+
+    @Autowired
+    public MessageQueueController(NotificationServiceImpl notificationService) {
+        this.service = notificationService;
+    }
+
+    @GetMapping(path = "/welcome")
+    public String home() {
+        return "Welcome to [ Message Queue Service ] !";
+    }
+
+    @PostMapping(value = "/mq/send_cancel_message")
+    public boolean send_cancel_message(@RequestBody NotifyInfo info, @RequestHeader HttpHeaders headers) {
+        return service.sendNotification(info, headers, "cancel");
+    }
+
+    @PostMapping(value = "/mq/send_preserve_message")
+    public boolean send_preserve_message(@RequestBody NotifyInfo info, @RequestHeader HttpHeaders headers) {
+        return service.sendNotification(info, headers, "preserve");
+    }
+
+    @PostMapping(value = "/mq/send_preserve_other_message")
+    public boolean send_preserve_other_message(@RequestBody NotifyInfo info, @RequestHeader HttpHeaders headers) {
+        return service.sendNotification(info, headers, "preserve_other");
+    }
+
 }
